f8f8fcc4f9bd1fb8b8022eada96ff6073419f501
mao <santichai.o@gmail.com>
2024-06-27 19:43:42 +0700

    servo

--- bird_detect.py
+++ bird_detect.py
@@ -2,14 +2,42 @@ import torch
 import argparse
 import time
 import cv2
+import serial
 from pathlib import Path
 from models.common import DetectMultiBackend
 from utils.dataloaders import LoadStreams
-from utils.general import check_img_size, non_max_suppression, scale_boxes, print_args
+from utils.general import (check_img_size, non_max_suppression, scale_boxes, print_args)
 from utils.torch_utils import select_device
 from ultralytics.utils.plotting import Annotator
 from utils.draw import draw_cross, draw_label, draw_circle
-from control import move
+
+# Define Arduino port
+arduino_port = "/dev/tty.usbserial-0001"
+arduino_baudrate = 9600
+
+def initialize_serial(port=arduino_port, baudrate=arduino_baudrate):
+    try:
+        ser = serial.Serial(port, baudrate)
+        time.sleep(2)  # Wait for the serial connection to initialize
+        return ser
+    except serial.SerialException as e:
+        print(f"Failed to connect to serial port: {e}")
+        return None
+
+def send_angles_to_arduino(ser, angleX, angleY):
+    try:
+        if ser is not None and ser.is_open:
+            ser.write(f'{angleX},{angleY}\n'.encode())
+    except serial.SerialException as e:
+        print(f"Failed to write to serial port: {e}")
+        ser.close()
+        ser = initialize_serial()  # Reinitialize serial connection
+    return ser
+
+def reset_angles(ser):
+    """Reset the angles to 90,90."""
+    angleX, angleY = 90, 90
+    send_angles_to_arduino(ser, angleX, angleY)
 
 def run(
     weights='yolov5s.pt',  # model.pt path(s)
@@ -22,6 +50,16 @@ def run(
     # half=False,  # use FP16 half-precision inference
     disp=True # show window
 ):
+    # Initialize serial connection to Arduino
+    ser = initialize_serial()
+    if ser is None:
+        print("Serial connection could not be established. Exiting.")
+        return
+
+    angleX, angleY = 90, 90
+
+    send_angles_to_arduino(ser, angleX, angleY)
+
     # Convert source to string if it's not
     source = str(source)
 
@@ -58,15 +96,18 @@ def run(
         print(f"Inference time: {inference_time:.3f} seconds")
 
         # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=[14], agnostic=False)  # class 14 is for birds
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=[39], agnostic=False, max_det=3)  # class 14 is for birds
+        # pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
         # Process predictions
         for i, det in enumerate(pred):  # per image
             p, im0 = path[i], im0s[i].copy()
             p = Path(p)  # to Path
             h, w, _ = im0.shape
-            center_x = w // 2
-            center_y = h // 2
+
+            # Calculate angles to move the camera
+            frame_center_x = w // 2
+            frame_center_y = h // 2
 
             if disp:
                 annotator = Annotator(im0, line_width=line_thickness, example=str(names))
@@ -75,55 +116,74 @@ def run(
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
 
-                # Find the largest bounding box
-                largest_box = None
+                # Find the largest detection
                 max_area = 0
+                max_det = None
                 for *xyxy, conf, cls in det:
                     x1, y1, x2, y2 = xyxy
                     area = (x2 - x1) * (y2 - y1)
-                    if conf > 0.6 and area > max_area:
+                    if conf > 0.3 and area > max_area:
                         max_area = area
-                        largest_box = (xyxy, conf, cls)
+                        max_det = (x1, y1, x2, y2, conf, cls)
 
-                if largest_box:
-                    # Draw the largest bounding box and add cross at its center
-                    xyxy, conf, cls = largest_box
+                if max_det:
+                    x1, y1, x2, y2, conf, cls = max_det
                     c = int(cls)  # integer class
                     label = f"{names[c]} {conf:.2f}"
-
-                    # Calculate center of the largest bounding box
-                    mcenter_x = int((xyxy[0] + xyxy[2]) / 2)
-                    mcenter_y = int((xyxy[1] + xyxy[3]) / 2)
-
-                    move((center_x, center_y), (mcenter_x, mcenter_y))
 
                     # Print the label and center coordinates in the console
                     if disp:
                         # annotator.box_label(xyxy, label, color=(0, 255, 0))
-                        draw_label(im0, label, xyxy)
+                        draw_label(im0, label, (int(x1), int(y1)))
 
-                        if abs(center_x - mcenter_x) < 10 and abs(center_y - mcenter_y) < 10 :
-                            draw_circle(im0, (center_x, center_y), radius=15)
+                        # Calculate the center of the largest detection
+                        center_x = (x1 + x2) // 2
+                        center_y = (y1 + y2) // 2
+
+                        dx, dy = abs(frame_center_x - center_x), abs(frame_center_y - center_y)
+                        if dx < 10 and dy < 10:
+                            draw_circle(im0, (frame_center_x, frame_center_y), radius=15)
                         else:
-                            draw_cross(im0, (mcenter_x, mcenter_y))
+                            draw_cross(im0, (int(center_x), int(center_y)))
+
+                            # Calculate angles (this part may need adjustment based on your setup) (320, center_x 356.0), angleX 90, angleY 76 (320, center_x 155.0), angleX 90, angleY 75
+                            if dx > 10:
+                                if frame_center_x - center_x > 0 and angleX < 180:
+                                    angleX += 1
+                                elif frame_center_x - center_x < 0 and angleX > 0:
+                                    angleX -= 1
+
+                            # if dy < 20:
+                            #     if frame_center_y - center_y > 0 and angleY < 180:
+                            #         angleY -= 1
+                            #     elif frame_center_y - center_y < 0 and angleY > 0:
+                            #         angleY += 1
+
+                            # print(f"frame_center_x ({frame_center_x}, center_x {center_x}), angleX {angleX}, angleY {angleY}")
+
+                            send_angles_to_arduino(ser, angleX, angleY)
+
+                            # time.sleep(0.5)
                     # else:
                     #     print(f"Detected: {label} at center ({center_x}, {center_y})")
-            else:
-                move((center_x, center_y))
 
             # Stream results
             if disp:
                 # Draw cross at center
-                draw_cross(im0, (center_x, center_y), color=(0, 255, 0))
+                draw_cross(im0, (frame_center_x, frame_center_y), color=(0, 255, 0))
 
                 im0 = annotator.result()
                 cv2.imshow(str(p), im0)
 
                 if cv2.waitKey(1) == ord('q'):  # Press 'q' to quit
+                    reset_angles(ser)
+
                     break
 
     if disp:
         cv2.destroyAllWindows()
+
+    reset_angles(ser)  # Reset angles when exiting
 
 def parse_opt():
     """Parses command-line arguments for YOLOv5 detection, setting inference options and model configurations."""
--- utils/draw.py
+++ utils/draw.py
@@ -5,9 +5,9 @@ def draw_cross(im, center, color=(0, 0, 255), thickness=1, size=10):
     cv2.line(im, (center_x - size, center_y), (center_x + size, center_y), color, thickness)
     cv2.line(im, (center_x, center_y - size), (center_x, center_y + size), color, thickness)
 
-def draw_label(im, label, xyxy, color=(0, 0, 255), thickness=1):
-    x1, y1 = int(xyxy[0]), int(xyxy[1])
-    cv2.putText(im, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
+def draw_label(im, label, center, color=(0, 0, 255), thickness=1):
+    center_x, center_y = center
+    cv2.putText(im, label, (center_x, center_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
 
 def draw_circle(im, center, color=(0, 0, 255), thickness=1, radius=10):
     cv2.circle(im, center, radius=radius, color=color, thickness=thickness )
